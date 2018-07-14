aa8698a2dd6ab1555d6ea06a9508ed2073edf3ea
Admin <cm.chandus333@gmail.com>
2018-07-14 08:37:52 +0530

    Intial project setup

---
+++ NHSC/NHSC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NHSC
+//
+//  Created by Admin on 14/07/18.
+//  Copyright © 2018 myspace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
